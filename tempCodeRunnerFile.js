1224370cc2fdeef6b798daf9fab1d65a490f55de
Smita <114800220+Smita-2004@users.noreply.github.com>
2024-02-07 12:26:39 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+function parentFunction(name, callback)
+// {
+//     callback();
+//     console.log("Hey "+name);
+// }
+// // this is callback function
+// function randomFunction()
+// {
+//     console.log("Hey I am callback Function");
+// }
+
+// parentFunction("Random String" ,randomFunction);
