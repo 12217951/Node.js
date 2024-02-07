a533c0dc0aaeff5b7fd5c224ff25648fe2bae7b6
Smita <114800220+Smita-2004@users.noreply.github.com>
2024-02-07 12:26:22 +0530

    Add files via upload

---
+++ timers.js
@@ -0,0 +1,7 @@
+// setTimeout Functon
+const  delayedFunction = () => {    //delayed function is declared with '=>'
+    console.log("Delayed function executed !");
+    
+};
+ 
+setTimeout(delayedFunction,2000);  //Executes after 2 sec
