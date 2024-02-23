cd0e65093e473ec78cffe8ed94ebdfecc803c70b
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-02-23 08:44:36 +0530

    Add files via upload

---
+++ server/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+const http= require('http');
+// const fs=require('fs');
+// const server = http.createServer((req,res)=>{
+//     if(url.req=="/"){
+//     const message="Hello , this file is written using Sync!";
+//     try{
+//         fs.writeFileSync('FileSystem/first.txt',message,'utf8');
+//         res.end('File-content:'+message);
+//     }
+//     catch(err){
+//         console.error(err);
+//         console.end("Server Error");
+//     }
+// }
+// }).listen
